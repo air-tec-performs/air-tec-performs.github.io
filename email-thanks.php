f3788922f366452c46590dd1c6110552613ecd3d
air-tec-performs <69865218+air-tec-performs@users.noreply.github.com>
2020-09-04 08:00:12 -0700

    Kurt Email Demo

---
+++ email-thanks.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="News Page for Air-Tec">
+  <meta name="author" content="SeanLiam McCarthy">
+
+  <title>Air-Tec News Page</title>
+
+  <!--  CSS -->
+  <link href="css/Air-Tec -Mariana.css" rel="stylesheet">
+  <link href="css/overall.css" rel="stylesheet">
+  <!--
+    Bootstrap Credit, acidentally renamed classes so commented out
+    <link href="vendor/bootstrap/css/bootstrap.css" rel="stylesheet">
+       
+    <link href="css/business-casual.min.css" rel="stylesheet">
+
+ -->
+  <!-- Font from Google API (from googling what makes a website look professional) -->
+  <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Playfair+Display:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i">
+
+</head>
+
+<body>
+    <div class="title">
+        <div class="title-first center-text caps">Air-Tec</div>
+        <div class="title-second center-text caps"></div>
+        <div class="title-third center-text caps">Performs</div>
+    </div>
+
+    <!-- NAV BAR -->
+    <div class="nav-height">
+        <div id="nav">
+            <div class="mobile-header caps center-text">Air-Tec</div>
+            <ul>
+                <li class="item center-text">
+                    <a href="https://www.airtecperforms.com/">Website</a>
+                </li>
+                <li class="item center-text">
+                    <a href="index.html">Home</a>
+                </li>
+                <li class="item center-text">
+                    <a href="news.html">News</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+
+    <div class="content-container">   
+        <div class="section">
+            <div class="section-content center-text">
+                <div class="secion-header-first center-text caps"></div>
+            <div class="secion-header-second center-text caps">Success.</div>
+            <p>
+                Your service request has been processed.  Thank you for taking the time to reach out to us.  If you would like to contact us immediately, please do so with 
+                the contact information that you will have recieved from the auto-email sent to you.
+            </p>
+            </div>
+        </div>
+    </div>
+
+  <footer class="footer">
+      <div class="footer-content center-text">Copyright © Air Tec Performs 2020</div>
+      <div class="footer-content center-text">Clay Dunn Enterprises, Inc.</div>
+  </footer>
+
+
+</body>
+
+</html>
